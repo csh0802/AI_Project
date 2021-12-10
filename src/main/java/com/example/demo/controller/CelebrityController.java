b91e8bfb776ee9ba7d3bf9329bd20bc2e3009e37
차성호 <91453151+csh0802@users.noreply.github.com>
2021-12-10 16:58:45 +0900

    [12-10] 연예인 사진 크롤링 추가

--- src/main/java/com/example/demo/controller/CelebrityController.java
+++ src/main/java/com/example/demo/controller/CelebrityController.java
@@ -23,24 +23,7 @@ public class CelebrityController {
 	
 	@Autowired
 	CelebrityDetectionService clerbrityDetection;
-	
-	@PostMapping("celebrityDetect")
-	@ResponseBody
-	public String celebrityDetect(MultipartFile image) {
 
-		System.out.println(image.getOriginalFilename());
-		try {
-			File uploadFile = new File("C:\\temp2\\" + image.getOriginalFilename());
-			image.transferTo(uploadFile);
-			return clerbrityDetection.celebrityDetect(uploadFile);
-
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return "upload fail!!!";
-		}
-
-	}
 	
 	@PostMapping("celeImg")
 	@ResponseBody   
